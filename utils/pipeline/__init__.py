48c6e1006b3b9b86532051a303d90783b80bb7e9
Leena Alghamdi <linaagh98@gmail.com>
2025-07-02 08:06:19 +0300

    Update __init__.py

--- utils/pipeline/__init__.py
+++ utils/pipeline/__init__.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
-# @Time    : 2021/5/31
-# @Author  : Lart Pang
-# @GitHub  : https://github.com/lartpang
+#Author: Lart Pang (https://github.com/lartpang) 
 
 from .optimizer import construct_optimizer
 from .scaler import Scaler
