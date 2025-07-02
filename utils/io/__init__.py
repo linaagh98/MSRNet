fbc54267834cc92d54dea2dda47e01a664f3dd84
Leena Alghamdi <linaagh98@gmail.com>
2025-07-02 08:04:53 +0300

    Update __init__.py

--- utils/io/__init__.py
+++ utils/io/__init__.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
-# @Time    : 2021/5/17
-# @Author  : Lart Pang
-# @GitHub  : https://github.com/lartpang
+#Author: Lart Pang (https://github.com/lartpang) 
 
 from .image import read_color_array, read_gray_array
 from .params import load_weight, save_weight
